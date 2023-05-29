98748c3b13cad640d6c140f2174d021b6645efe8
nmargenov <nikolay_margenov@yahoo.com>
2023-05-29 23:52:57 +0300

    setup database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const connectToDb = require('./config/databaseConfig');
 
 
 const app = express();
 
 expressConfig(app);
 handlebarsConfig(app);
+
+connectToDb()
+    .then(console.log('Connection to DB is successful!'))
+    .catch(err =>{console.log('Error connecting to the DB: ',err)});
 
 app.get('/',(req,res)=>{
     res.write('Server is working properly!');

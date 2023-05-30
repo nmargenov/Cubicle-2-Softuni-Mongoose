3e6a092b34438f4b544f9b50a09b787d306538df
nmargenov <nikolay_margenov@yahoo.com>
2023-05-30 18:37:29 +0300

    Search now works(with filter not with query)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,11 +4,26 @@ const router = require('express').Router();
 
 
 router.get(['/','/index'],async (req,res)=>{
+    
+    const {search,from,to} = req.query;
+
+    
+    let cubes = await getAllCubes().lean();
+    
+    if(search){
+        cubes = cubes.filter(cube=>cube.name.toLowerCase().includes(search.trim().toLowerCase()));
+    }
+
+    if(from){
+        cubes = cubes.filter(cube=>cube.difficultyLevel>=Number(from));
+    }
+
+    if(to){
+        cubes = cubes.filter(cube=>cube.difficultyLevel<=Number(to));
+    }
+
     res.status(302);
-
-    const cubes = await getAllCubes().lean();
-
-    res.render('index',{cubes});
+    res.render('index',{cubes,search,from,to});
 });
 router.get('/about',(req,res)=>{
     res.status(302);
